6a7d62fed9c1ba967fb961e32c5aeafe72439c8c
unknown <nguyenhuuminh24101998@gmail.com>
2019-05-22 15:17:11 +0700

    bai 1 update

--- bai1/bai1.php
+++ bai1/bai1.php
@@ -1,71 +1,54 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    <title>Giải phương trình bậc hai</title>
+<!doctype html>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <?php
-            if(isset($_POST['tinh']))
-            {
-                if(isset($_POST['a']))
-                {$a=$_POST['a'];}
-                if(isset($_POST['b']))
-                {$b=$_POST['b'];}
-                if(isset($_POST['c']))
-                {$c=$_POST['c'];}
-                function giaiptbac2($a,$b,$c)
-                {
-
-                    if($a == 0)
-                    {
-                        if($b == 0)
-                        { echo ("Phương trình vô nghiệm");}
-                        else
-                        echo"Phươnq trình có 1 nghiệm là "." x = " .(-$c / $b);
-                    }
-                    else {
-                        $delta = $b * $b - 4 * $a * $c;
-                        $x1 = "";
-                        $x2 = "";
-                        if ($delta > 0) {
-                            $x1 = -$b + sqrt($delta) / 2 * $a;
-                            $x2 = -$b - sqrt($delta) / 2 * $a;
-                            echo "<br>";
-                            echo "Phuong trinh co 2 nghiem la " . " x1= " . $x1 . ", x2= " . $x2;
-                        }
-                        else if($delta==0)
-                        {
-                            echo "<br>";
-                            echo "Phuong trinh co nghiem kep x1 = x2 =".$x1;
-                        }
-                        else
-                        {
-                            echo "<br>";
-                            echo "Phuong trinh vo nghiem";
-                        }
-                    }
-                }
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Giải pt bậc 2</h1>
+<form method="post" action="">
+    <input type="text" name="a" style="width: 20px" value="<?php echo isset($_POST['a']) ? $_POST['a'] : '' ?>">
+    x<sup>2</sup>
+    + <input type="text" name="b" style="width: 20px" value="<?php echo isset($_POST['b']) ? $_POST['b'] : '' ?>">x
+    + <input type="text" name="c" style="width: 20px" value="<?php echo isset($_POST['c']) ? $_POST['c'] : '' ?>"> = 0
+    <input type="submit" name="tinh" value="Tính">
+</form>
+<?php
+$kq = '';
+$x1 = '';
+$x2 = '';
+if (isset($_POST['tinh'])) {
+    $a = isset($_POST['a']) ? $_POST['a'] : '';
+    $b = isset($_POST['b']) ? $_POST['b'] : '';
+    $c = isset($_POST['c']) ? $_POST['c'] : '';
+    if ($a == '' || $b == '' || $c == '')
+        die("Bạn hãy nhập đầy đủ thông tin");
+    if (is_numeric($a) && is_numeric($b) && is_numeric($c)) {
+        if ($a == 0) {
+            if ($b == 0 && $c != 0)
+                echo "Phương trình vô nghiệm";
+            else {
+                die("Phương trình đã cho có 1 nghiệm x= " . (-$c / $b));
             }
-        ?>
-        <h1>Giải pt bậc 2</h1>
-        <form method="post" action="#">
-            <input type="text" name="a" style="width: 20px" value="<?=$a?>">x <sup>2</sup>
-            +<input type="text" name="b" style="width: 20px" value="<?=$b?>">x
-            +<input type="text" name="c" style="width: 20px" value="<?=$c?>"> = 0
-            <br/><br/>
-            <input type="submit" name="tinh" value="Tính">
-        </form>
-        <?php
-            if(is_numeric($GLOBALS ['a'])&& is_numeric($GLOBALS ['b'])&& is_numeric($GLOBALS ['c']))
-            {
-                giaiptbac2($GLOBALS ['a'],$GLOBALS['b'],$GLOBALS ['c']);
-            }
-            else
-            {
-                echo "Input khong hop le";
-            }
-        ?>
-    </body>
+        }
+        $delta = $b * $b - 4 * $a * $c;
+        $x1 = (-$b + sqrt($delta)) / (2 * $a);
+        $x2 = (-$b - sqrt($delta)) / (2 * $a);
+        if ($delta > 0) {
+            $kq = "Phương trình có 2 nghiệm phân biệt x1 =" . $x1 . ", x2 = " . $x2;
+        } else if ($delta == 0) {
+            $kq = "Phương trình có ngiệm kép x1 = x2 =" . $x1;
+        } else {
+            $kq = "Phương trình vô nghiệm";
+        }
+    } else {
+        $kq = "Input không đúng định dạng  .";
+    }
+}
+echo $kq;
+?>
+</body>
 </html>
