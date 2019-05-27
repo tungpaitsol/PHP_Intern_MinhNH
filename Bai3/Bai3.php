d63adde339135b177614c6fd505a650433eb5814
unknown <nguyenhuuminh24101998@gmail.com>
2019-05-27 11:10:14 +0700

    Fix function rand_int

--- Bai3/Bai3.php
+++ Bai3/Bai3.php
@@ -15,40 +15,33 @@
 </form>
 <?php session_start() ?>
 <?php
-$a=$_POST['a'] ? $_POST['a'] : '';
-$array=array();
-if(isset($_POST['create']))
-{
-    if (isset($_POST['a']) && is_numeric($a) && $a > 0)
-    {
-        for ($i = 0; $i < $a; $i++)
-        {
-            $type=rand(0,1);
-            if($type==0)
-                $array[$i]=rd_string(round(rand($a/4,(3*$a/4))));
+$a = $_POST['a'] ? $_POST['a'] : '';
+$array = array();
+if (isset($_POST['create'])) {
+    if (isset($_POST['a']) && is_numeric($a) && $a > 0) {
+        for ($i = 0; $i < $a; $i++) {
+            $type = rand(0, 1);
+            if ($type == 0)
+                $array[$i] = rd_string(ceil(rand($a / 4, (3 * $a / 4))));
             else
-                $array[$i]=rd_int(round(rand($a/4,(3*$a/4))));
+                $array[$i] = rd_int(ceil(rand($a / 4, (3 * $a / 4))));
         }
         echo '<pre>';
         var_dump($array);
-        $_SESSION['array']=$array;
-    }
-    else
-    {
+        $_SESSION['array'] = $array;
+    } else {
         echo "Input sai định dạng";
     }
 }
-if(isset($_POST['distribute']))
-{
-    $array=$_SESSION['array'];
-    $array_int=array();
-    $array_string=array();
-    foreach ($array as $item)
-    {
+if (isset($_POST['distribute'])) {
+    $array = $_SESSION['array'];
+    $array_int = array();
+    $array_string = array();
+    foreach ($array as $item) {
         if (is_int($item))
-            $array_int[]=$item;
+            $array_int[] = $item;
         else
-            $array_string[]=$item;
+            $array_string[] = $item;
     }
     echo '<pre>';
     var_dump($array_int);
@@ -57,26 +50,23 @@ if(isset($_POST['distribute']))
 }
 function rd_int($lenght)
 {
-    $chars = '0123456789';
-    $size= strlen($chars);
-    $str='';
-    for($i=0; $i<$lenght ; $i++)
-    {
-        $str .=$chars[rand(0,$size -1)];
-    }
-    return (int)$str;
+    $min_num = pow(10, $lenght - 1);
+    $max_num = pow(10, $lenght) - 1;
+    $number = rand($min_num, $max_num);
+    return (int)$number;
 }
+
 function rd_string($lenght)
 {
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    $size= strlen($chars);
-    $str='';
-    for($i=0; $i<$lenght ; $i++)
-    {
-        $str .=$chars[rand(0,$size -1)];
+    $size = strlen($chars);
+    $str = '';
+    for ($i = 0; $i < $lenght; $i++) {
+        $str .= $chars[rand(0, $size - 1)];
     }
     return $str;
 }
+
 ?>
 </body>
 </html>
